dea47b162ecb078a6cbdcc85abcdc28184975da0
Aditya Hansda <40536327+adityahansda@users.noreply.github.com>
2024-03-29 10:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def main():
                 st.success("Done")
 
     # Main content area for displaying chat messages
-    st.title("Chat with PDF files using Gemini🤖")
+    st.title("Chat with PDF files 🤖 by Aditya Hansda")
     st.write("Welcome to the chat!")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
